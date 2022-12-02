106305be754341339ad42f4f221bf710f0dcc665
ines <insanlur@gmail.com>
2022-12-02 10:11:13 +0100

    Reorder variables

--- 2022/2/solution.py
+++ 2022/2/solution.py
@@ -8,22 +8,26 @@ replacements = {
     "Y": "1",
     "Z": "2"
 }
+draw_points = 3
+win_points = 6
 games = raw_input.translate(raw_input.maketrans(replacements)).split("\n")
 games = [map(int, game.split()) for game in games]
 
-score_1, score_2 = 0, 0
+scores = [0, 0]
 for game in games:
     their_move, my_move = game
+    winning_move = (their_move + 1) % 3
+    losing_move = (their_move - 1) % 3
     # Part 1
-    if my_move == their_move: score_1 += 3 # draw
-    elif my_move == (their_move + 1) % 3: score_1 += 6 # win
-    score_1 += my_move + 1 # lose
+    if my_move == their_move: scores[0] += draw_points # draw
+    elif my_move == winning_move: scores[0] += win_points # win
+    scores[0] += my_move + 1
     # Part 2 -> second column is not my move but the match result
-    if my_move == 0: score_2 += (their_move - 1) % 3 # lose
-    elif my_move == 2: score_2 += 6 + (their_move + 1) % 3 # win
-    else: score_2 += 3 + their_move # draw
-    score_2 += 1
+    if my_move == 0: scores[1] += losing_move # lose
+    elif my_move == 2: scores[1] += win_points + winning_move # win
+    else: scores[1] += draw_points + their_move # draw
+    scores[1] += 1
 
 # Answers
-print(f"In part one my total score would be {score_1}")
-print(f"In part two my total score would be {score_2}")
+print(f"In part one my total score would be {scores[0]}")
+print(f"In part two my total score would be {scores[1]}")
