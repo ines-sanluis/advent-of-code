e6470def75d3bb5d63b425cd23a6206c4254780f
ines <insanlur@gmail.com>
2022-12-02 07:36:47 +0100

    Fix var names

--- 2022/1/solution.py
+++ 2022/1/solution.py
@@ -8,7 +8,7 @@ print(f"Elf number {best_elf} is carrying the most calories with a total of {mos
 
 # Part 2
 best_three = sorted_elves_calories[-3:]
-second_helf = elves_calories.index(best_three[1]) + 1
-third_helf = elves_calories.index(best_three[0]) + 1
+second_elf = elves_calories.index(best_three[1]) + 1
+third_elf = elves_calories.index(best_three[0]) + 1
 
-print(f"Elfs {best_elf}, {second_helf} and {third_helf} are carrying a total of {sum(best_three)} calories")
+print(f"Elves {best_elf}, {second_elf} and {third_elf} are carrying a total of {sum(best_three)} calories")
