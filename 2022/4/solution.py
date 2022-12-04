07f290af5748498251213063733034474a3fa4bb
Ines San Luis <47610074+ines-sanluis@users.noreply.github.com>
2022-12-04 10:06:40 +0100

    Update solution.py

--- 2022/4/solution.py
+++ 2022/4/solution.py
@@ -10,10 +10,10 @@ for pairs in assignments:
     first_range = create_range(pair[0]) # range of first elf
     second_range = create_range(pair[1]) # range of second elf
     # Part One
-    if first_range.issubset(second_range) or second_range.issubset(first_range): # if one of the range is a subset of the other they are fully contained
+    if first_range.issubset(second_range) or second_range.issubset(first_range): # if one of the ranges is a subset of the other then they are fully contained
         fully_contained += 1
     # Part two
-    if len(first_range & second_range) > 0: # if the intersection has at least one element is because they overlap
+    if len(first_range & second_range) > 0: # if the intersection has at least one element then ranges overlap
         overlaps += 1
 
 # Answers
