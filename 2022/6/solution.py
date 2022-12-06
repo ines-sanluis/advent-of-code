d4e961b3c3ba1540b8f201d1f75d84cc9a5ed33e
Ines San Luis <47610074+ines-sanluis@users.noreply.github.com>
2022-12-06 13:24:25 +0100

    Update solution.py

--- 2022/6/solution.py
+++ 2022/6/solution.py
@@ -1,11 +1,10 @@
-datastream = open("input.txt").read()
-
 def get_start_of_marker(datastream, packet_size):
     for i in range(len(datastream)):
         sequence = datastream[i : i + packet_size]
         if len(set(sequence)) == packet_size:
             return i + packet_size
 
+datastream = open("input.txt").read()
 # Part One
 print(f"Number of characters processed before the first start-of-packet marker: {get_start_of_marker(datastream, 4)}")
 # Part Two
